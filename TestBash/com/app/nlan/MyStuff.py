4c9740520a8971ce959dec6749ed4c25029a0c38
Nicolas Lanatta <nicolaslanatta@gmail.com>
2016-10-15 17:20:49 -0300

    This is a test for the new gui

--- TestBash/com/app/nlan/MyStuff.py
+++ TestBash/com/app/nlan/MyStuff.py
@@ -8,17 +8,29 @@ class MyStuff(object):
         self.tangerine = "And now a thousand years between"
 
     def execute(self):
-        # write stocks data as comma-separated values
-        writer = csv.writer(open('stocks.csv', 'wb', buffering=0))
-        writer.writerows([
-            ('GOOG', 'Google, Inc.', 505.24, 0.47, 0.09),
-            ('YHOO', 'Yahoo! Inc.', 27.38, 0.33, 1.22),
-            ('CNET', 'CNET Networks, Inc.', 8.62, -0.13, -1.49)
-        ])
+        BOARD_SIZE = 8
 
-        # read stocks data, print status messages
-        stocks = csv.reader(open('stocks.csv', 'rb'))
-        status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
-        for ticker, name, price, change, pct in stocks:
-            status = status_labels[cmp(float(change), 0.0)]
-            print '%s is %s (%s%%)' % (name, status, pct)
+        def under_attack(col, queens):
+            left = right = col
+
+            for r, c in reversed(queens):
+                left, right = left - 1, right + 1
+
+                if c in (left, col, right):
+                    return True
+            return False
+
+        def solve(n):
+            if n == 0:
+                return [[]]
+
+            smaller_solutions = solve(n - 1)
+
+            return [solution + [(n, i + 1)]
+                    for i in xrange(BOARD_SIZE)
+                    for solution in smaller_solutions
+                    if not under_attack(i + 1, solution)]
+
+        for answer in solve(BOARD_SIZE):
+            print answer
+
